8434b304c254d857e88663a1dde8f3edf6b5db3c
Erick <erick.kuwahara79@gmail.com>
2023-08-10 18:00:03 -0300

    style: quiz result section

--- js/script.js
+++ js/script.js
@@ -495,6 +495,21 @@ nextQuestion.addEventListener("click", () => {
   }
 });
 
+// RESULT PROGRESS
+
+const meters = document.querySelectorAll("svg[data-value] .meter");
+
+meters.forEach((path) => {
+  let length = path.getTotalLength();
+  let value = parseInt(path.parentNode.getAttribute("data-value"));
+  let to = length * ((100 - value) / 100);
+
+  path.getBoundingClientRect();
+
+  path.style.strokeDashoffset = Math.max(0, to);
+  path.nextElementSibling.textContent = `${value / 20}/5`;
+});
+
 // START SESSION
 
 function startSession() {
