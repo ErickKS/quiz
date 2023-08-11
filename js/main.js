065330a383ac48d32c893ce61f809f9e884ba18d
Erick <erick.kuwahara79@gmail.com>
2023-08-11 19:01:05 -0300

    feat: quizzes functionality

---
+++ js/main.js
@@ -0,0 +1,56 @@
+const body = document.querySelector("body");
+const preLoader = document.getElementById("preLoader");
+
+const registerSection = document.getElementById("registerSection");
+const registerInput = document.getElementById("registerInput");
+const registerButton = document.getElementById("registerButton");
+
+const mainSection = document.getElementById("main");
+
+// ========== PRE LOADER for MAIN PAGE
+function loadSession() {
+  setTimeout(() => {
+    body.style.overflowY = "visible";
+    preLoader.style.display = "flex";
+    registerSection.style.display = "none";
+
+    if (preLoader.style.opacity == 0) {
+      setTimeout(() => {
+        preLoader.style.display = "none";
+        mainSection.style.display = "block";
+      }, 1500);
+    }
+  }, 100);
+}
+
+// ========== START SESSION
+function startSession() {
+  loadSession();
+
+  const nickName = registerInput.value;
+  const nickPlace = document.getElementById("nickname");
+
+  nickPlace.innerHTML = "Hi " + nickName;
+}
+
+// ========== REGISTER
+registerInput.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    event.preventDefault();
+
+    registerButton.click();
+  }
+});
+
+registerButton.addEventListener("click", (event) => {
+  if (registerInput.value === "") {
+    registerInput.classList.add("alert");
+  } else {
+    alert("Application being rebuilt");
+    // startSession();
+  }
+});
+
+// ========== LOG OUT
+const exitButton = document.getElementById("exitButton");
+exitButton.addEventListener("click", () => location.reload());
---
+++ js/quiz.js
@@ -0,0 +1,163 @@
+// ========== QUIZ MAIN
+import { cinema } from "./mock.js";
+
+const quizCinema = document.getElementById("quizCinema");
+const quizSports = document.getElementById("quizSports");
+const quizTech = document.getElementById("quizTech");
+const quizScience = document.getElementById("quizScience");
+const quizMath = document.getElementById("quizMath");
+const quizKnowledge = document.getElementById("quizKnowledge");
+const quizMusic = document.getElementById("quizMusic");
+const quizGeography = document.getElementById("quizGeography");
+
+const quizSection = document.getElementById("quiz");
+const quizResultSection = document.getElementById("quizResult");
+
+const question = document.getElementById("question");
+const options = document.querySelectorAll("input[type=radio]");
+const a_label = document.getElementById("a_label");
+const b_label = document.getElementById("b_label");
+const c_label = document.getElementById("c_label");
+const d_label = document.getElementById("d_label");
+const nextQuestion = document.getElementById("nextQuestion");
+
+let quiz = null;
+let currentQuizData = null;
+let currentQuestion = 0;
+let scoreQuiz = 0;
+
+function showQuiz() {
+  quizSection.style.display = "flex";
+  quizSection.style.opacity = 1;
+  quizSection.style.visibility = "visible";
+
+  options.forEach((selected) => {
+    selected.addEventListener("change", () => {
+      nextQuestion.removeAttribute("disabled");
+      nextQuestion.style.cursor = "pointer";
+    });
+  });
+}
+
+function loadQuiz() {
+  setTimeout(() => {
+    quizSection.style.display = "flex";
+    body.style.overflowY = "hidden";
+    preLoader.style.display = "flex";
+
+    if (preLoader.style.opacity == 0) {
+      setTimeout(() => {
+        preLoader.style.display = "none";
+        mainSection.style.display = "none";
+        quizSection.style.display = "flex";
+      }, 1000);
+    }
+  }, 100);
+}
+
+function transitionQuizAnimation() {
+  setTimeout(() => {
+    quizSection.style.opacity = 0;
+    quizSection.style.visibility = "hidden";
+    nextQuestion.style.pointerEvents = "none";
+
+    if (quizSection.style.opacity == "0") {
+      setTimeout(() => {
+        quizSection.style.opacity = 1;
+        quizSection.style.visibility = "visible";
+        nextQuestion.style.pointerEvents = "initial";
+      }, 400);
+    }
+  }, 100);
+}
+
+function replaceTextQuiz() {
+  question.innerText = currentQuizData.question;
+  a_label.firstElementChild.nextElementSibling.textContent = currentQuizData.a_option;
+  b_label.firstElementChild.nextElementSibling.textContent = currentQuizData.b_option;
+  c_label.firstElementChild.nextElementSibling.textContent = currentQuizData.c_option;
+  d_label.firstElementChild.nextElementSibling.textContent = currentQuizData.d_option;
+}
+
+function backToMenu() {
+  currentQuestion = 0;
+  scoreQuiz = 0;
+
+  quizSection.style.display = "none";
+  quizResultFruitSection.style.display = "none";
+  quizResultSection.style.display = "none";
+
+  loadSession();
+}
+
+function disableNextQuestionButton() {
+  nextQuestion.setAttribute("disabled", true);
+  nextQuestion.style.cursor = "not-allowed";
+}
+
+function deselectAlternatives() {
+  options.forEach((selected) => {
+    selected.checked = false;
+  });
+}
+
+nextQuestion.addEventListener("click", () => {
+  let answer = null;
+  options.forEach((selected) => {
+    if (selected.checked) {
+      answer = selected.id;
+    }
+  });
+
+  if (answer === quiz[currentQuestion].correctOption) {
+    scoreQuiz++;
+  }
+
+  currentQuestion++;
+
+  transitionQuizAnimation();
+  disableNextQuestionButton();
+  deselectAlternatives();
+
+  switch (currentQuizData.theme) {
+    case "cinema":
+      setTimeout(() => {
+        loadQuizCinema();
+      }, 500);
+      break;
+  }
+});
+
+function loadQuizCinema() {
+  showQuiz();
+
+  console.log(currentQuestion, scoreQuiz);
+  quiz = cinema;
+  currentQuizData = quiz[currentQuestion];
+  replaceTextQuiz();
+}
+quizCinema.addEventListener("click", () => {
+  loadQuiz();
+  loadQuizCinema();
+});
+
+// ========== QUIZ RESULT
+const resultMessage = document.getElementById("resultMessage");
+const resultScore = document.getElementById("resultScore");
+
+const repeatButton = document.getElementById("repeatButton");
+const backToMenuButton = document.getElementById("backToMenuButton");
+
+const meters = document.querySelectorAll("svg[data-value] .meter");
+
+// SCORE ANIMATION
+meters.forEach((path) => {
+  let length = path.getTotalLength();
+  let value = parseInt(path.parentNode.getAttribute("data-value"));
+  let to = length * ((100 - value) / 100);
+
+  path.getBoundingClientRect();
+
+  path.style.strokeDashoffset = Math.max(0, to);
+  path.nextElementSibling.textContent = `${value / 20}/5`;
+});
